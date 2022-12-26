db875127079a1b6be102481b7f85559369546f1d
Kunal <kunalsonawane730@gmail.com>
2022-12-26 08:54:04 +0530

    Add: validating set Event for employee payroll application

--- js/empForm.js
+++ js/empForm.js
@@ -1,7 +1,33 @@
-//UC8 : Ability to Set Event Listener on Salary Range to display appropriate value 
-const salary=document.querySelector('#salary');
-const output=document.querySelector('.salary-output');
-output.textContent=salary.value;
-salary.addEventListener('input',function(){
-    output.textContent=salary.value;
-    })
+window.addEventListener('DOMContentLoaded', (event) => {
+    salaryOutput();
+    validateName();
+    //validateDate();
+
+});
+
+function salaryOutput() {
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function() {
+        output.textContent = salary.value;
+
+    });
+}
+
+function validateName() {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+}
